2d761e9773b753f3530c5de63d77978aef2f09c2
isaacdev07 <isaacsantos7953@gmail.com>
2026-03-17 20:03:02 -0300

    Feat: add necessary query to minimize memory usage

--- src/main/java/com/study/card/repository/CardRepository.java
+++ src/main/java/com/study/card/repository/CardRepository.java
@@ -1,9 +1,16 @@
 package com.study.card.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.study.card.model.Card;
+import com.study.card.model.enums.Status;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
     
+    // método para encontrar cards expirados
+    List<Card> findByStatusAndEndDateBefore(Status status, LocalDate data);
+
 }
