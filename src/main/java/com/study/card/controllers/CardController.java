c8e5c6399bc7b587f9d23a393bbda93c418e8689
isaacdev07 <isaacsantos7953@gmail.com>
2026-03-17 20:03:56 -0300

    Feat: new endpoints based on service

--- src/main/java/com/study/card/controllers/CardController.java
+++ src/main/java/com/study/card/controllers/CardController.java
@@ -1,5 +1,7 @@
 package com.study.card.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.study.card.dtos.CardDTO;
 import com.study.card.model.Card;
 import com.study.card.service.CardSevice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,6 +26,7 @@ public class CardController {
     @Autowired
     private CardSevice cardSevice;
 
+    // criar novo card
     @PostMapping
     public ResponseEntity<Card> createCard(@RequestBody CardDTO cardDTO) {
         
@@ -27,6 +34,21 @@ public class CardController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCard);
     }
+
+    // marcar card como concluído
+    @PatchMapping("/{id}/concluir")
+    public ResponseEntity<Card> markAsCompleted(@PathVariable Long id) {
+        Card updatedCard = cardSevice.completeCard(id);
+        return ResponseEntity.ok(updatedCard);
+    }
     
+    // get todos os cards
+    @GetMapping
+    public ResponseEntity<List<Card>> getAllCards() {
+        List<Card> cards = cardSevice.getAllCards();
+        
+        // Retorna a lista de cards com o status 200 OK
+        return ResponseEntity.ok(cards);
+    }
     
 }
