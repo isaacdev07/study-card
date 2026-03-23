803423806abd61ccf9029614d54d858b4261ffa5
isaacdev07 <isaacsantos7953@gmail.com>
2026-03-23 19:50:33 -0300

    Feat: add validations to fields

--- src/main/java/com/study/card/model/User.java
+++ src/main/java/com/study/card/model/User.java
@@ -1,9 +1,11 @@
 package com.study.card.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -15,8 +17,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "O nome não pode ser nulo")
     private String name;
+    @Column(unique = true)
+    @NotNull(message = "O email não pode ser nulo")
     private String email;
+    @NotNull(message = "A senha não pode ser nula")
     private String password;
     
 }
