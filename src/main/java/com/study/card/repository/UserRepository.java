184230c8c9af811f63532de1ef9055b148fde603
isaacdev07 <isaacsantos7953@gmail.com>
2026-03-23 19:53:16 -0300

    Feat: add method to find by email

--- src/main/java/com/study/card/repository/UserRepository.java
+++ src/main/java/com/study/card/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.study.card.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.study.card.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     
+    Optional<User> findByEmail(String email);
+
 }
