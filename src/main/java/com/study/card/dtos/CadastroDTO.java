4bf6b5bc319a1fbe8061c446843c8b88fd5a9926
isaacdev07 <isaacsantos7953@gmail.com>
2026-03-23 19:54:54 -0300

    Feat: dto to create new user

---
+++ src/main/java/com/study/card/dtos/CadastroDTO.java
@@ -0,0 +1,12 @@
+package com.study.card.dtos;
+
+import lombok.Data;
+
+@Data
+public class CadastroDTO {
+    // classe para cadastro de usuário
+    private String name;
+    private String email;
+    private String password;
+    
+}
