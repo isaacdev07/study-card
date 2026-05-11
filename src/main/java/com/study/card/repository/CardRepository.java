bae34b0994216d6af88849e1acebec680d271359
isaacdev07 <isaacsantos7953@gmail.com>
2026-05-10 23:15:45 -0300

    Feat: add method to find cards by user in CardRepository

--- src/main/java/com/study/card/repository/CardRepository.java
+++ src/main/java/com/study/card/repository/CardRepository.java
@@ -6,11 +6,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.study.card.model.Card;
+import com.study.card.model.User;
 import com.study.card.model.enums.Status;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
     
     // método para encontrar cards expirados
     List<Card> findByStatusAndEndDateBefore(Status status, LocalDate data);
+
+    List<Card> findByUser(User user);
 
 }
