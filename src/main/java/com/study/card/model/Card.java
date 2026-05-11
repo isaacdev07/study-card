1a76027d0146a7bebdeb68ee3d9e701d8d41f086
isaacdev07 <isaacsantos7953@gmail.com>
2026-05-10 23:15:32 -0300

    Feat: add user association to Card model

--- src/main/java/com/study/card/model/Card.java
+++ src/main/java/com/study/card/model/Card.java
@@ -2,11 +2,14 @@ package com.study.card.model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.study.card.model.enums.Status;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -14,6 +17,11 @@ import lombok.Data;
 @Entity
 @Table(name = "cards")
 public class Card {
+
+    @ManyToOne // anotacão para indicar que muitos cards podem pertencer a um usuário
+    @JoinColumn(name = "user_id") 
+    @JsonIgnore // evita looping infinito durante a serialização JSON
+    private User user;
     
     // classe card
     @Id
